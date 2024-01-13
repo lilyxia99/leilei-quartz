e6b3f395ff68e23ad7583563ec64910702b8f4b7
lilyxia99 <lxia01@risd.edu>
2024-01-13 18:52:01 -0500

    vault backup: 2024-01-13 18:52:01

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,12 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+  footer: Component.MobileOnly(Component.Explorer({title: "Menu",folderDefaultState: "collapsed",useSavedState: true,})),
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -33,7 +28,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.MobileOnly(Component.Explorer({title: "Menu",folderDefaultState: "collapsed",useSavedState: true,})),
   ],
 }
 
