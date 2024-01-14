fe90cd76a3f10f0254c3a1553e648c8f0e9cd8e5
lilyxia99 <lxia01@risd.edu>
2024-01-14 10:20:25 -0500

    vault backup: 2024-01-14 10:20:24

--- quartz.config.ts
+++ quartz.config.ts
@@ -53,7 +53,7 @@ const config: QuartzConfig = {
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting(),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
