df9545e76a0473a209dafb3a767468cc878a4bc8
lilyxia99 <lilyxia999@gmail.com>
2024-01-22 23:31:12 -0500

    vault backup: 2024-01-22 23:31:12

---
+++ content/Collection/watch-list/Animation/Panthon.md
@@ -0,0 +1,5 @@
+---
+tags: 
+time: 2024-01-22
+---
+ ![Panthon](https://www.youtube.com/watch?v=DGaS9ih8YTU)
--- quartz/components/Comment.tsx
+++ quartz/components/Comment.tsx
@@ -1,12 +1,29 @@
-import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types";
+import { useState } from 'preact/hooks'; // Import useState if you are using functional components
 
 function Comments({ fileData, displayClass }: QuartzComponentProps) {
+  const [commentText, setCommentText] = useState(''); // Using useState to manage the comment text
+
+  // Function to handle input changes
+  const handleInputChange = (event: Event) => {
+    const target = event.target as HTMLInputElement; // Type casting to HTMLInputElement
+    setCommentText(target.value); // Updating state
+  };
+
+  // Function to handle button click
+  const handleButtonClick = () => {
+    alert('Button clicked');
+    console.log('Comment:', commentText); // Logging the comment
+    // Add further logic to process the commentText
+  };
+
   return (
     <div class={`comments ${displayClass ?? ""}`}>
       <h3>Comments</h3>
-      <input></input>
+      <input type="text" onInput={handleInputChange} value={commentText}></input>
+      <button type="button" onClick={()=>console.log("yes")}>Comment</button>
     </div>
-  )
+  );
 }
 
-export default (() => Comments) satisfies QuartzComponentConstructor
+export default (() => Comments) satisfies QuartzComponentConstructor;
