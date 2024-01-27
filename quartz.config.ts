2481684c05a253a4bb05e6c103cb764aa5d40926
lilyxia99 <lilyxia999@gmail.com>
2024-01-26 22:12:05 -0500

    vault backup: 2024-01-26 22:12:05

--- quartz.config.ts
+++ quartz.config.ts
@@ -47,7 +47,7 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
-      Plugin.TableOfContents(),
+      Plugin.TableOfContents({collapseByDefault: true}),
       Plugin.CreatedModifiedDate({
         // you can add 'git' here for last modified from Git
         // if you do rely on git for dates, ensure defaultDateType is 'modified'
